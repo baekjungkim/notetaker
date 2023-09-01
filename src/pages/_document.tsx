8267f9bff27bce5a131530894c6094b865db5bce
Kim Baekjung <zvgandam@gmail.com>
2023-09-01 11:27:03 +0900

    Adding the whole app

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html data-theme="garden">
+      <Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Head>
+    </Html>
+  );
+}
